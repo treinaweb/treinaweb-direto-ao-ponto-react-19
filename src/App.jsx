78a2df22b7159bc8d6699e690a042f8053424231
Wesley Gado <wesleygado@gmail.com>
2024-09-09 20:06:36 -0300

    06 - hook useActionState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import FormState from './components/FormState'
 import UserForm from './components/UserForm'
 
 function App() {
   return (
-    <UserForm />
+    <FormState />
   )
 }
 
---
+++ src/components/FormState.jsx
@@ -0,0 +1,52 @@
+import React, { useActionState } from "react";
+
+const FormState = () => {
+  const validarUsuario = (prevState, formData) => {
+    const name = formData.get("name");
+    const age = formData.get("age");
+
+    if (age >= 18) {
+      return {
+        success: true,
+        name,
+        text: "Idade Permitida!"
+      }
+    } else {
+      return {
+        success: false,
+        name,
+        text: "Você deve ter mais de 18 anos de idade!"
+      }
+    }
+  };
+
+  const [message, actionForm] = useActionState(validarUsuario, null)
+
+  return (
+    <form action={actionForm}>
+      <label>Nome:</label>
+      <br />
+      <input
+        type="text"
+        name="name"
+      />
+      <br />
+      <br />
+      <label>Idade: </label>
+      <br />
+      <input
+        type="number"
+        name="age"
+      />
+      <br />
+      <br />
+      <button type="submit">Enviar</button>
+      <br />
+      <br />
+      {message && <h3>{`Olá ${message.name}!`}</h3>}
+      {message && <h3>{message.text}</h3>}
+    </form>
+  );
+};
+
+export default FormState;
